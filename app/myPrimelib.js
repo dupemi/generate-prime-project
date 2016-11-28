a32ae1fb63e36c4fbfdd50340e07032e1f4b5406
ADEGOKE MEDINAT <metmeddy@yahoo.co.uk>
2016-11-28 17:10:12 +0100

    new commit

--- app/myPrimelib.js
+++ app/myPrimelib.js
@@ -1,3 +1,4 @@
+/*module.exports = getPrimes;
 var getPrimes = module.exports = function(n){
 	var arriPrime = [];
 	for(var i = 2; i <= n ; i++){
@@ -16,3 +17,28 @@ var getPrimes = module.exports = function(n){
     }
     return arriPrime;
 };
+*/
+
+var confirmPrimes = function(n){
+ var isPrime = [];
+ if (n < 2 ){
+   return false;
+ }
+ for(var i=2; i<n; i++) {
+   if (n%i === 0 ){
+     isPrime.push(i);
+   }
+ }
+ return isPrime.length === 0;
+};
+
+var getPrimes = function(n){
+ var primes = [];
+ for(var i=1; i<=n; i++){
+   if(confirmPrimes(i)){
+     primes.push(i);
+   }
+ }
+ return primes;
+};
+module.exports = getPrimes;
